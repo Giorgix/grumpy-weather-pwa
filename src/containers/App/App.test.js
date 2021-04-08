cc852e6c993406f93860b8556f0e7c2ccc2481c2
Jorge MD Ramos <jorgemdramos@gmail.com>
2021-04-08 11:45:37 +0200

    test(*): add locationSlice, Weather and App tests

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,8 +1,66 @@
-import { render, screen } from '@testing-library/react';
+import { waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import fetchMock from 'fetch-mock';
+import { render, fireEvent, screen } from '../../test-utils';
 import App from '.';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/loading/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  afterEach(() => {
+    fetchMock.restore()
+  })
+  const initialState = {
+    location: {
+      value: {
+        data: {
+          current_lat: 44,
+          current_lon: 3.4,
+        },
+        loading: true,
+        completed: false,
+        current_loading: true,
+        current_completed: false,
+      }
+    }
+  }
+  let store,wrapper
+
+  it('Shows Spinner on first load', () => {
+    render(<App />,{initialState})
+
+    expect(screen.getByText(/spinner loading/i)).not.toBeNull()
+  })
+  it('Shows Weather after loading', async() => {
+    fetchMock.get('https://api.opencagedata.com/geocode/v1/json?q=44+3.4&key=f62e33ffb4294e3cb537350fde241077', {
+      results: [
+        {
+          geometry: {
+            lat: 44,
+            lng: 3.5
+          },
+          components: {
+            city: 'Madrid',
+            quarter: 'Goya'
+          }
+        }
+      ],
+      headers: { 'content-type': 'application/json' }
+    })
+    fetchMock.get('http://api.openweathermap.org/data/2.5/weather?lat=44&lon=3.5&units=metric&APPID=8e69078d04cbc142a30de0c0456fe417', {
+      main: {
+        temp: '30'
+      },
+      weather: [
+        {
+          description: 'clear skies'
+        }
+      ],
+      wind: '30kmh',
+      name: 'Madrid centro',
+      headers: { 'content-type': 'application/json' }
+    })
+    render(<App />, {initialState});
+    const tempElement = await waitFor(() => screen.getByText(/30/i), {timeout: 3000})
+    expect(tempElement).toBeInTheDocument();
+  })
+})
